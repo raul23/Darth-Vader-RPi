7c42c5e86ed1b93273139060791b66734d314345
raul23 <rchfe23@gmail.com>
2020-07-06 23:59:08 -0400

    Add script

---
+++ script.py
@@ -0,0 +1,148 @@
+import threading
+import time
+
+# import ipdb
+
+import pygame
+import RPi.GPIO as GPIO
+
+
+def run_leds_sequence(leds):
+	t = threading.currentThread()
+	seq_idx = 0
+	sequence = [[leds['top'], leds['bottom']],
+				[leds['top']],
+				[leds['bottom']],
+				[leds['middle'], leds['bottom']],
+				[leds['middle']],
+				[leds['top'], leds['middle']],
+				[leds['top'], leds['middle'], leds['bottom']]]
+	while getattr(t, "do_run", True):
+		list_leds = sequence[seq_idx % len(sequence)]
+		seq_idx += 1
+		off(leds['top'])
+		off(leds['middle'])
+		off(leds['bottom'])
+		for led in list_leds:
+			on(led)
+		time.sleep(2)
+	print("Stopping thread")
+
+
+def off(n):
+	print("LED {} off".format(n))
+	GPIO.output(n, GPIO.LOW)
+	
+
+def on(n):
+	print("LED {} on".format(n))
+	GPIO.output(n, GPIO.HIGH)
+
+ 
+def start():
+	print("Starting...")
+	
+	print("RPi initialization...")
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	# LEDs
+	top_led = 11
+	middle_led = 9
+	bottom_led = 10
+	GPIO.setup(top_led, GPIO.OUT)
+	GPIO.setup(middle_led, GPIO.OUT)
+	GPIO.setup(bottom_led, GPIO.OUT)
+	GPIO.setup(22, GPIO.OUT)
+	# Buttons
+	GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+	print("pygame initialization...")
+	pygame.init()
+	pygame.mixer.init()
+	
+	### Sound
+	# Create separate channel
+	# Ref.: stackoverflow.com/a/59742418
+	channel1 = pygame.mixer.Channel(0)
+	channel1.set_volume(0.2)
+	channel2 = pygame.mixer.Channel(1)
+	channel3 = pygame.mixer.Channel(3)
+	
+	print("Loading songs...")
+	
+	# Darth Vader breathing sound
+	breathing_sound = pygame.mixer.Sound('darth_vader_breathing_GOOD.ogg')
+	channel1.play(breathing_sound, -1)  # loop indefinitely
+	
+	# Lightsaber sounds
+	lightsaber_open_sound = pygame.mixer.Sound('lightsaber_darth_vader_opening.ogg')
+	lightsaber_running_sound = pygame.mixer.Sound('lightsaber_darth_vader_running.ogg')
+	lightsaber_close_sound = pygame.mixer.Sound('lightsaber_darth_vader_retraction.ogg')
+	
+	# Imperial March song
+	# imperial_march_song = pygame.mixer.Sound('song_the_imperial_march.ogg')
+	
+	# Darth Vader quotes
+	quotes = []
+	quotes.append(pygame.mixer.Sound('quote_i_am_your_father_1.ogg'))
+	quotes.append(pygame.mixer.Sound('quote_i_am_your_father_2_with_music_at_the_end.ogg'))
+	quotes.append(pygame.mixer.Sound('quote_dont_make_me_destroy_you.ogg'))
+	quotes.append(pygame.mixer.Sound('quote_give_yourself_to_the_dark_side.ogg'))
+	quotes.append(pygame.mixer.Sound('quote_if_you_only_knew_the_power_of_the_dark_side.ogg'))
+	quotes.append(pygame.mixer.Sound('quote_nooooo.ogg'))
+	quotes.append(pygame.mixer.Sound('quote_there_is_no_escape.ogg'))
+	quotes.append(pygame.mixer.Sound('quote_your_lack_of_faith_is_disturbing.ogg'))
+	
+	leds = {'top': top_led, 'middle': middle_led, 'bottom': bottom_led}
+	th = threading.Thread(target=run_leds_sequence, args=(leds,))
+	th.start()
+	
+	print("Press any button")
+	pressed_lightsaber = False
+	quote_idx = 0
+	try:
+		while True:
+			if not GPIO.input(23):
+				print("Button 23 pressed...")
+				if pressed_lightsaber:
+					pressed_lightsaber = False
+					channel3.play(lightsaber_close_sound)
+					time.sleep(0.3)
+					off(22)
+				else:
+					pressed_lightsaber = True
+					channel3.play(lightsaber_open_sound)
+					channel3.play(lightsaber_running_sound, -1)
+					time.sleep(0.3)
+					on(22)
+				time.sleep(0.2)
+			elif not GPIO.input(24):
+				print("Button 24 pressed...")
+				channel2.play(imperial_march_song)
+				time.sleep(0.2)
+			elif not GPIO.input(25):
+				print("Button 25 pressed...")
+				quote = quotes[quote_idx % len(quotes)]
+				quote_idx += 1
+				channel2.play(quote)
+				time.sleep(0.2)		
+	except Exception as e:
+		print("Error: ", e)
+	except KeyboardInterrupt:
+		print("\nExiting...")
+		
+	print("Cleanup")
+	th.do_run = False
+	th.join()
+	off(top_led)
+	off(middle_led)
+	off(bottom_led)
+	off(22)
+	GPIO.cleanup()
+	channel1.stop()
+	channel2.stop()
+	channel3.stop()
+
+start()
