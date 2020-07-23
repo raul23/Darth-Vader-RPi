96fcd708c36005b31e7ab00f2ec901edb75060c3
Raul <rchfe23@gmail.com>
2020-07-22 20:07:34 -0400

    Add utility functions

---
+++ darth_vader_rpi/utils.py
@@ -0,0 +1,39 @@
+import codecs
+import json
+import logging
+
+from collections import namedtuple
+from logging import NullHandler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(NullHandler())
+
+
+# TODO: use load_json() from pyutils
+def load_json(filepath, encoding='utf8'):
+    try:
+        with codecs.open(filepath, 'r', encoding) as f:
+            data = json.load(f)
+    except OSError:
+        raise
+    else:
+        return data
+
+
+def msg_with_spaces(msg, nb_spaces=20):
+    return "{}{}".format(msg, " " * nb_spaces)
+
+
+def override_config_with_args(config, args):
+    retval = namedtuple("retval", "args_not_found config_opts_overidden")
+    retval.args_not_found = []
+    retval.config_opts_overidden = []
+    for k, new_v in args.__dict__.items():
+        old_v = config.get(k)
+        if old_v is not None:
+            if new_v != old_v:
+                config[k] = new_v
+                retval.config_opts_overidden.append((k, old_v, new_v))
+        else:
+            retval.args_not_found.append(k)
+    return retval
