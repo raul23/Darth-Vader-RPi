8892eb8aab2533958258b5bb10513922f1b18fe9
Raul <rchfe23@gmail.com>
2020-09-13 02:52:11 -0400

    __init__: update doc

--- darth_vader_rpi/__init__.py
+++ darth_vader_rpi/__init__.py
@@ -1,4 +1,7 @@
-"""Package TODO
+"""**Darth-Vader-RPi** is a Python-based Raspberry Pi (RPi) project about
+activating a Darth Vader action figure by turning on LEDs on his suit and
+lightsaber, and by playing sounds such as some of his famous quotes.
+
 """
 # For debugging purposes
 __test_version__ = "0.0.0a0"
