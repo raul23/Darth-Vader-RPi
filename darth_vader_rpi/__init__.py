0d245601c55e6ca4cd561fbaeb743a6067847c6c
Raul <rchfe23@gmail.com>
2020-09-22 00:49:11 -0400

    Update version to 0.1.0a0.dev1

--- darth_vader_rpi/__init__.py
+++ darth_vader_rpi/__init__.py
@@ -6,4 +6,4 @@ lightsaber, and by playing sounds such as some of his famous quotes.
 # For debugging purposes
 __test_version__ = "0.0.0a0"
 # Version of package
-__version__ = "0.1.0a0"
+__version__ = "0.1.0a0.dev1"
